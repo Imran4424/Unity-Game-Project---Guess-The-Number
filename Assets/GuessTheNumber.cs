b721a4fd279e0a880ece6aefa7cacfb69ac25fe8
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-25 16:07:53 +0600

    Guess the number

---
+++ Assets/GuessTheNumber.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GuessTheNumber : MonoBehaviour {
+
+	public InputField input;
+	public Text infoText;
+
+	private int guessnumber;
+	private int userguess;
+
+	// Use this for initialization
+	void Start () {
+		guessnumber = Random.Range (0, 100);
+	}
+
+	public void checkGuess () {
+		userguess = int.Parse (input.text);
+
+		if (userguess == guessnumber) {
+			infoText.text = "You Guessed The Number! You are a Wizard!!!";
+		} 
+		else if (userguess > guessnumber) 
+		{
+			infoText.text = "Your Number Is Greater Than The Guess Number";
+		} 
+		else 
+		{
+			infoText.text = "Your Number Is Less Than The Guess Number";
+		}
+	}
+
+}
