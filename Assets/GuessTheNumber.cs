4b20080b829f93fa7cb7fb83ed5d91a4651301aa
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-27 15:09:21 +0600

    Guess the number

--- Assets/GuessTheNumber.cs
+++ Assets/GuessTheNumber.cs
@@ -35,7 +35,7 @@ public class GuessTheNumber : MonoBehaviour {
 	}
 
 	IEnumerator wait () {
-		yield return new WaitForSeconds (5);
+		yield return new WaitForSeconds (3);
 
 		SceneManager.LoadScene ("Scene0", LoadSceneMode.Single);
 	} 
