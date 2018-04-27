5a77de1fa01652f070ce215585162864a07d882c
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-27 14:51:00 +0600

    Guess the number

--- Assets/GuessTheNumber.cs
+++ Assets/GuessTheNumber.cs
@@ -25,7 +25,7 @@ public class GuessTheNumber : MonoBehaviour {
 
 			StartCoroutine (wait ());
 
-			SceneManager.Load
+			SceneManager.LoadS
 
 		} else if (userguess > guessnumber) {
 			infoText.text = "Your Number Is Greater Than The Guess Number";
