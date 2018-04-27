ce7762c98d50a8d01338f9489b83a6e5925061a2
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-27 14:33:14 +0600

    Guess the number

--- Assets/GuessTheNumber.cs
+++ Assets/GuessTheNumber.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Sce
+using UnityEngine.SceneManagement
 
 public class GuessTheNumber : MonoBehaviour {
 
